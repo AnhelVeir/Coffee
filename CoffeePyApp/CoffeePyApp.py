2f3b3673b58d113ce087a1720e9c2192d4d69c9e
AnhelVeir <izolda00000@gmail.com>
2020-06-30 17:55:23 +0400

    Обновлен дизайн. Упрощение кода

--- CoffeePyApp/CoffeePyApp.py
+++ CoffeePyApp/CoffeePyApp.py
@@ -18,15 +18,16 @@ Builder.load_string("""
         Rectangle:
             pos: self.pos
             size: self.size
-            source: '.data//fon.png'
+            source: '.data//coffee_wall.jpg'
     FloatLayout:
         Label:
             id: title_label
-            text: "[b][color=#A6622B]Coffee Company[/color][/b]"
+            #text: "[b][color=#A6622B]Coffee Company[/color][/b]"
+            text: "[b]Coffee Company[/b]"
             markup: True
-            font_size: '24sp'
+            font_size: '28sp'
             size_hint: (.6, .1)
-            pos_hint: {'x':.2, 'y':.8}
+            pos_hint: {'x':.2, 'y':.85}
         Label:
             text: "[i][color=#3E3A37]Учебная практика - Гайдомак Мария (ИСП-925)[/color][/i]"
             markup: True
@@ -42,8 +43,8 @@ Builder.load_string("""
             pos_hint: {'x':.2, 'y':.15} 
         Button:
             id: enter_button
-            size_hint: (.21, .08)
-            pos_hint: {'x':.255, 'y':.3}
+            size_hint: (.21, .06)
+            pos_hint: {'x':.2, 'y':.4}
             text: 'Войти'
             background_color: (.94, .66, .39, 1)
             background_normal: ''
@@ -60,8 +61,8 @@ Builder.load_string("""
                 root.release()  
         Button:
             id: register_button
-            size_hint: (.26, .08)
-            pos_hint: {'x':.48, 'y':.3}
+            size_hint: (.26, .06)
+            pos_hint: {'x':.42, 'y':.4}
             text: 'Зарегистироваться'
             background_color: (.94, .66, .39, 1)
             background_normal: ''
@@ -83,12 +84,12 @@ Builder.load_string("""
             size_hint: (None, None)
             halign: 'left'
             size: self.texture_size
-            pos_hint:{'x':.25, 'y':.67} 
+            pos_hint:{'x':.2, 'y':.67} 
         TextInput:
             id: login_enter
             text: ''
-            size_hint:(.5, .06)
-            pos_hint:{'x':.25, 'y':.6}
+            size_hint:(.48, .05)
+            pos_hint:{'x':.2, 'y':.6}
             multiline:False  
         Label:
             text:'[color=#231C0B]Пароль[/color]'
@@ -97,12 +98,12 @@ Builder.load_string("""
             size_hint: (None, None)
             halign: 'left'
             size: self.texture_size
-            pos_hint: {'x':.25, 'y':.52}
+            pos_hint: {'x':.2, 'y':.56}
         TextInput:
             id: passw_enter
             text: ''
-            size_hint: (.5, .06)
-            pos_hint: {'x':.25, 'y':.45}
+            size_hint: (.48, .05)
+            pos_hint: {'x':.2, 'y':.5}
             multiline: False
             password : True
 
@@ -246,7 +247,7 @@ Builder.load_string("""
             size_hint: (.1, .05)
             pos_hint: {'x':.8, 'y':.1} 
             on_press:
-                root.manager.current = 'main'
+                app.info = root.exit(app.user, app.info)
         Label:
             id: info_label
             text: ''
@@ -273,6 +274,7 @@ Builder.load_string("""
 <ReportScreen>:
     on_enter:
         root.enter_screen(app.user)
+
     canvas.before:
         Rectangle:
             pos: self.pos
@@ -489,7 +491,7 @@ Builder.load_string("""
             markup: True
             font_size: '16sp'
             size_hint: (.6, .1)
-            pos_hint: {'x':.2, 'y':.03} 
+            pos_hint: {'x':.2, 'y':.015} 
         Button:
             text: 'Назад'
             size_hint: (.1, .05)
@@ -596,6 +598,12 @@ def restore_color(button):
 
 
 def plot(df, img, title=False):
+    """
+    Принимает df, обьект Image для размещения графика, title (bool)
+    Строит график по столбцам date (x), liters (y)
+    title - наличие названия графика.
+    :return: None
+    """
     plt.figure(figsize=(10, 5), tight_layout=True)
     ax = plt.subplot(111)
     sns.set(style="darkgrid")
@@ -611,6 +619,20 @@ def plot(df, img, title=False):
     img.source = ".data\\partplot.png"
     img.opacity = 1
 
+
+def sort_df(df):
+    """
+    Принимает df, сортирует по столбцу 'date'
+    :return: df
+    """
+    df['date'] = df['date'].apply(lambda x: datetime.strptime(x, "%d/%m/%y"))
+    df = df.sort_values(by=['date'])
+    df['date'] = df['date'].apply(lambda x: x.strftime("%d/%m/%y"))
+    df = df.reset_index()
+    del df['index']
+    df.index.name = 'index'
+    df.to_csv('.data\\info.csv', sep=';')
+    return df
 
 class LoginScreen(Screen):
 
@@ -736,7 +758,7 @@ class AddnoteScreen(Screen):
         else:
             self.ids.info_label.text = ''
             self.date = self.date.strftime("%d/%m/%y")
-            if self.ids.liters.text == "":
+            if self.ids.liters.text == '':
 
                 self.ids.info_label.text = '[color=#DD1B07]Введите объем[/color]'
             else:
@@ -747,12 +769,6 @@ class AddnoteScreen(Screen):
                 except IndexError:
                     self.last_index = info.count()[0]
                     info.loc[self.last_index] = {'user': user, 'date': self.date, 'liters': float(self.ids.liters.text)}
-                    info['date'] = info['date'].apply(lambda x: datetime.strptime(x, "%d/%m/%y"))
-                    info = info.sort_values(by=['date'])
-                    info['date'] = info['date'].apply(lambda x: x.strftime("%d/%m/%y"))
-                    info = info.reset_index()
-                    del info['index']
-                    info.index.name = 'index'
                     info.to_csv('.data\\info.csv', sep=';')
                     self.ids.info_label.text = ""
 
@@ -765,6 +781,10 @@ class AddnoteScreen(Screen):
         finally:
             self.ids.date.text = '01/01/20'
             self.ids.liters.text = '0.0'
+
+    def exit(self, user, info):
+        self.manager.current = 'main'
+        return sort_df(df=info)
 
 
 class ReportScreen(Screen):
@@ -779,6 +799,20 @@ class ReportScreen(Screen):
         self.ids.report_text.text = "..."
         self.ids.plot.source = ""
         self.ids.plot.opacity = 0
+
+    def sort_df(self, df):
+        """
+        Принимает df, сортирует по столбцу 'date'
+        :return: None
+        """
+        df['date'] = df['date'].apply(lambda x: datetime.strptime(x, "%d/%m/%y"))
+        df = df.sort_values(by=['date'])
+        df['date'] = df['date'].apply(lambda x: x.strftime("%d/%m/%y"))
+        df = df.reset_index()
+        del df['index']
+        df.index.name = 'index'
+        df.to_csv('.data\\info.csv', sep=';')
+        return df
 
     def give_report(self, user, info):
         """
@@ -998,7 +1032,7 @@ class CoffeeApp(App):
         self.title = 'Coffee Company'
         self.icon = '.data\\myicon.png'
         Window.clearcolor = (.94, .82, .75, 1)
-        Window.size = (800, 600)
+        Window.size = (900, 700)
         self.user = ''
         self.users = pd.read_csv('.data\\users.csv', sep=';', index_col=[0])
         self.info = pd.read_csv('.data\\info.csv', sep=';', index_col=[0])
